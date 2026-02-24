92506b8b09d373a334a713530b6d9a83bd6a4091
larissa-felixl <larissa.lima145@aluno.ce.gov.br>
2026-02-24 08:07:22 -0300

    feat(Spending_limits): Migration de limite de gastos mensais pela secretaria.

---
+++ app/Http/Controllers/SpendingLimitController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpendingLimit;
+use Illuminate\Http\Request;
+
+class SpendingLimitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SpendingLimit $spendingLimit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SpendingLimit $spendingLimit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SpendingLimit $spendingLimit)
+    {
+        //
+    }
+}
